7a975b53fd86c10db03aceff0854a694df28819d
Agnes <Agnes@Letsapp.local>
2022-07-05 11:19:56 +0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </div>
+
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
